f9ec73edc9cdccef77082ce7667c3ce64580c8cd
AstrodevJV <soalejandro@protonmail.com>
2025-03-16 11:53:39 -0500

    chore: agregar dependencias

--- src/main/java/com/example/drepslogin/controller/UsuarioController.java
+++ src/main/java/com/example/drepslogin/controller/UsuarioController.java
@@ -5,10 +5,8 @@ import com.example.drepslogin.model.UsuarioModel;
 import com.example.drepslogin.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
