4f1cdef453c93e941fe7d802f3e87217b2d79326
AstrodevJV <soalejandro@protonmail.com>
2025-03-16 10:55:19 -0500

    feat: implementar método para buscar usuario por ID

--- src/main/java/com/example/drepslogin/controller/UsuarioController.java
+++ src/main/java/com/example/drepslogin/controller/UsuarioController.java
@@ -6,10 +6,12 @@ import com.example.drepslogin.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/usuarios")
@@ -26,4 +28,14 @@ public class UsuarioController {
     public List<UsuarioModel> findAll() {
         return usuarioService.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioModel> findById(@PathVariable Long id) {
+        Optional<UsuarioModel> usuarioModel = usuarioService.findById(id);
+        if (usuarioModel.isPresent()) {
+            return ResponseEntity.ok(usuarioModel.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 }
